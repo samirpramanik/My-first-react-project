d718b46864e7d2969d2ceecaddc4d378f5e0580e
samirpramanik <samirpramanikjsr@gmail.com>
2025-01-26 19:52:44 +0400

    Initial commit

---
+++ reactproject/src/components/Hello.js
@@ -0,0 +1,12 @@
+const name = "TestName";
+function Hello() {
+  return <h1>Hello {displayMessage()}</h1>;
+}
+
+//const Hello = () => <h1>Hello World!</h1>
+
+const displayMessage = () => {
+    return "Child function!";
+}
+
+export default Hello;
---
+++ reactproject/src/components/Message.js
@@ -0,0 +1,10 @@
+import {Component} from 'react';
+class Message extends Component {
+  render() {
+    return (
+      <h1>This is a class component!</h1>
+    );
+  }
+}
+
+export default Message;
